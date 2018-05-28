1e00a9c5aaccc542b74b7b6ac7f48b1a127792a6
hanyuaa <370065056@qq.com>
2018-05-28 11:09:08 +0800

    日产提交

--- src/main/java/com/manning/Demo_04.java
+++ src/main/java/com/manning/Demo_04.java
@@ -1,14 +1,25 @@
 package com.manning;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Created by hanyu on 2018/4/17.
  */
 public class Demo_04 {
 
     public static void main(String[] args) {
-        int abc = 1212;
+        HashSet<Integer> set = Sets.newHashSet();
+        ArrayList<Integer> integers = Lists.newArrayList(1, 2, 3, 4, 5);
+        List<Integer> collect = integers.stream().filter(e -> !set.contains(e)).collect(Collectors.toList());
 
-        int c = -abc;
-        System.out.println(c);
+        System.out.println(collect);
     }
+
 }
