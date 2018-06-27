c6cb9f9c4ebfac78dd37c79fa51633e6e99094cc
hanyuaa <370065056@qq.com>
2018-06-27 12:16:54 +0800

    test

--- src/main/java/com/manning/Demo_01.java
+++ src/main/java/com/manning/Demo_01.java
@@ -1,26 +1,7 @@
 package com.manning;
 
-import java.util.Arrays;
-
-/**
- * Created by hanyu on 2018/4/3.
- */
 public class Demo_01 {
     public static void main(String[] args) {
-        Day day = Day.MONDAY;
-        Day day1 = Enum.valueOf(Day.class, day.name());
-        Day[] days = Day.values();
-        Enum e = day;
-        Class aClass = e.getDeclaringClass();
-        boolean anEnum = aClass.isEnum();
-        Day[] enumConstants = (Day[]) aClass.getEnumConstants();
 
-        System.out.print(anEnum+" "+Arrays.toString(enumConstants));
     }
-}
-
-//定义枚举类型
-enum Day {
-    MONDAY,TUESDAY,WEDNESDAY,
-    THURSDAY,FRIDAY,SATURDAY,SUNDAY
 }
--- src/main/java/com/manning/ES_Demo.java
+++ src/main/java/com/manning/ES_Demo.java
@@ -2,6 +2,8 @@ package com.manning;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 
 /**
  *  Elasticsearch 测试
@@ -12,6 +14,12 @@ public class ES_Demo {
     private static ElasticsearchTemplate elasticsearchTemplate;
 
     public static void main(String[] args) {
-        //elasticsearchTemplate.
+        Book book = new Book();
+        book.setId(1l);
+        book.setAuthor("张三");
+
+        IndexQuery usertest = new IndexQueryBuilder().withIndexName("usertest").withId("1").withObject(book).build();
+        String index = ES_Demo.elasticsearchTemplate.index(usertest);
+        System.out.println(index);
     }
 }
